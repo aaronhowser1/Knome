48ff608b904e873e3dda474c900a972f30e1f010
aaron <aaronhowser1@gmail.com>
2026-01-16 00:56:06 -0600

    Refactor MongoDB connection settings for QuoteRepository

--- src/main/kotlin/dev/aaronhowser/apps/knome/feature/QuoteRepository.kt
+++ src/main/kotlin/dev/aaronhowser/apps/knome/feature/QuoteRepository.kt
@@ -1,14 +1,24 @@
 package org.example.dev.aaronhowser.apps.knome.feature
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import org.bson.Document
+import org.bson.UuidRepresentation
 
 object QuoteRepository {
 
-	private val mongoClient: MongoClient = MongoClients.create("mongodb://localhost:27017")
+	private val mongoClient: MongoClient = MongoClients.create(
+		MongoClientSettings.builder()
+			.applyConnectionString(ConnectionString("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"))
+			.applicationName("knome")
+			.uuidRepresentation(UuidRepresentation.STANDARD)
+			.build()
+	)
+
 	private val database: MongoDatabase = mongoClient.getDatabase("knome_bot")
 
 	val quotes: MongoCollection<Document> = database.getCollection("quotes")
