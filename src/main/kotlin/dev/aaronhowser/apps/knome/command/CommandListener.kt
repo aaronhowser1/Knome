199bbb89a10fb393e4e7498ea2ce595d8fb8b1f0
aaron <aaronhowser1@gmail.com>
2026-01-15 19:41:58 -0600

    Refactor Crosspost command handling and improve command registration

--- src/main/kotlin/dev/aaronhowser/apps/knome/command/CommandListener.kt
+++ src/main/kotlin/dev/aaronhowser/apps/knome/command/CommandListener.kt
@@ -6,32 +6,24 @@ import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.dv8tion.jda.api.interactions.commands.OptionType
-import net.dv8tion.jda.api.interactions.commands.build.Commands
 
 class CommandListener : ListenerAdapter() {
 
 	override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
-		if (event.name == CROSSPOST_COMMAND) {
-			event.deferReply(true).queue()
-			CoroutineScope(Dispatchers.IO).launch {
-				CrosspostCommand.handleCrosspost(event)
+		when (event.name) {
+			CrosspostCommand.COMMAND_NAME -> {
+				event.deferReply(true).queue()
+				CoroutineScope(Dispatchers.IO).launch {
+					CrosspostCommand.handleCrosspost(event)
+				}
 			}
 		}
 	}
 
 	override fun onReady(event: ReadyEvent) {
 		event.jda.updateCommands()
-			.addCommands(
-				Commands.slash(CROSSPOST_COMMAND, "Crosspost messages")
-					.addOption(OptionType.STRING, "start", "First message link", true)
-					.addOption(OptionType.STRING, "end", "Last message link", false)
-			)
+			.addCommands(CrosspostCommand.getCommand())
 			.queue()
-	}
-
-	companion object {
-		const val CROSSPOST_COMMAND = "crosspost"
 	}
 
 }
--- src/main/kotlin/dev/aaronhowser/apps/knome/command/CrosspostCommand.kt
+++ src/main/kotlin/dev/aaronhowser/apps/knome/command/CrosspostCommand.kt
@@ -1,10 +1,30 @@
 package org.example.dev.aaronhowser.apps.knome.command
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 
 object CrosspostCommand {
 
+	const val COMMAND_NAME = "crosspost"
+	const val START_ARGUMENT = "start"
+	const val END_ARGUMENT = "end"
+
+	fun getCommand(): SlashCommandData {
+		return Commands.slash(COMMAND_NAME, "Crosspost messages")
+			.addOption(OptionType.STRING, "start", "First message link", true)
+			.addOption(OptionType.STRING, "end", "Last message link", false)
+	}
+
 	fun handleCrosspost(event: SlashCommandInteractionEvent) {
+		val startLink = event.getOption(START_ARGUMENT)?.asString
+		val endLink = event.getOption(END_ARGUMENT)?.asString
+
+		if (startLink == null) {
+			event.hook.sendMessage("Start link is required.").queue()
+			return
+		}
 
 	}
 
